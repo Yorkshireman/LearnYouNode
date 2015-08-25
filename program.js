4498c0a3c6395427ef90c5e224fbc1bff9a4db2f
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-25 18:14:29 +0100

    'HTTP Collect' (Stage 8) complete

--- program.js
+++ program.js
@@ -4,6 +4,14 @@ var httpPath = process.argv[2];
 
 http.get(httpPath, function(response) {
   response.setEncoding('utf8');
-  response.on('data', console.log);
-  response.on('error', console.log);
+  
+  var text = '';
+  response.on('data', function(chunk) {
+    text += chunk;
+  });
+
+  response.on('end', function() {
+    console.log(text.length);
+    console.log(text);
+  });
 });
