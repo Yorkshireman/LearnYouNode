22dc6021f6403a29fc70b2c7ce132fe7dfe6e652
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-26 14:32:16 +0100

    setup loop, but results returning empty - see comments for Ben's hints

--- program.js
+++ program.js
@@ -1,4 +1,6 @@
 var http = require('http');
+var urlsArray = process.argv.slice(2);
+var results = [];
 
 function collectData(url, callback) {
   http.get(url, function(response) {
@@ -21,17 +23,32 @@ function printFinalResults() {
   }
 };
 
-var urlsArray = process.argv.slice(2);
-var results = [];
+function series(array) {
 
-function series(url) {
-  if(url) {
-    collectData(url, function(text) {
+  for (i = 0; i < array.length; i++) {
+    collectData(array[i], function(text) {
       results.push(text);
-      return series(urlsArray.shift());
     });
-  } else {
-    return printFinalResults();
   }
+  console.log(results); //returns empty!
 }
-series(urlsArray.shift());
+
+series(urlsArray);
+
+//it’s asynchrony again.  Remember, `results.push(text)` is within the callback that gets passed to `collectData` so the loop simply sets (`array.length`) number of asynchronous processes going, then immediately logs the contents of `results` which won’t have been populated yet.
+//yep - but take comfort - this is not an easy problem to solve
+
+// Ben [2:17 PM]
+// I’ll give you a clue.  You need to pass your sequencing function (in this case `series`) a callback to call with the final results.
+
+// Ben [2:18 PM]
+// Then, on each callback within the iteration, you need to determine if there are any other callbacks still waiting to return.  if not, you can call the final callback.
+
+// Ben [2:21 PM]
+// One way to do that is to include the value of `array[i]` as an extra parameter to the callback passed to `collectData`.  That way, when your callback is invoked, you can test to see if you’ve received callbacks for every element in the original array.
+
+// Ben [2:22 PM]
+// you may have to read that once or twice...
+
+// Ben [2:25 PM]
+// I would advise you to sort out your function and parameter naming as well as the problem is further complicated by cognitive overload
