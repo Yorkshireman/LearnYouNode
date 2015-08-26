0e36153d291678fa7a309a4c75b35fe73bda9174
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-26 13:03:36 +0100

    exercise 9 (solution, but not final version)

--- program.js
+++ program.js
@@ -1,17 +1,37 @@
-var http = require("http");
+var http = require('http');
 
-var httpPath = process.argv[2];
+function collectData(url, callback) {
+  http.get(url, function(response) {
+    response.setEncoding('utf8');
 
-http.get(httpPath, function(response) {
-  response.setEncoding('utf8');
-  
-  var text = '';
-  response.on('data', function(chunk) {
-    text += chunk;
+    var text = '';
+    response.on('data', function(chunk) {
+      text += chunk;
+    });
+
+    response.on('end', function() {
+      callback(text)
+    });
   });
+}
 
-  response.on('end', function() {
-    console.log(text.length);
-    console.log(text);
-  });
-});
+function printFinalResults() { 
+  for (i = 0; i < results.length; i++) {
+    console.log(results[i]);
+  }
+};
+
+var urlsArray = process.argv.slice(2);
+var results = [];
+
+function series(url) {
+  if(url) {
+    collectData(url, function(text) {
+      results.push(text);
+      return series(urlsArray.shift());
+    });
+  } else {
+    return printFinalResults();
+  }
+}
+series(urlsArray.shift());
